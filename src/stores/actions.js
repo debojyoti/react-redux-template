d9d51e3bb60a5c7a4ddbbeb2193e0d7ea418cb26
debo <debojyoti.js@gmail.com>
2024-04-06 20:32:38 +0530

    Added absolute path import vs code settings

--- src/stores/actions.js
+++ src/stores/actions.js
@@ -1,5 +1,5 @@
-import { store } from ".";
-import { setUsername } from "./user-slice";
+import { store } from "src/stores";
+import { setUsername } from "src/stores/user-slice";
 
 /**
  * This contains all the actions as fuctions, organized by the slice they belong to.
